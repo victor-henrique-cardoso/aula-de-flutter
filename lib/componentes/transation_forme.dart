5365702820d89dbcac60898eed9a613df349908b
victor henrique <victorcardosoferreirahenrique@gmail.com>
2025-02-22 20:06:28 -0400

    começando a fazer o modal pra adicionar as despesas

--- lib/componentes/transation_forme.dart
+++ lib/componentes/transation_forme.dart
@@ -1,11 +1,22 @@
 import 'package:flutter/material.dart';
 
 class TransationForme extends StatelessWidget {
-
   final titleController = TextEditingController();
   final valueController = TextEditingController();
-  final void Function(String,double) onSubmit;
+  final void Function(String, double) onSubmit;
   TransationForme(this.onSubmit);
+
+  _submitForm() {
+    final Title = titleController.text;
+    final value = double.parse(valueController.text) ?? 0.0;
+
+    if(Title.isEmpty || value <=0){
+      return;
+    }
+
+    onSubmit(Title, value);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Card(
@@ -16,22 +27,22 @@ class TransationForme extends StatelessWidget {
           children: [
             TextField(
               controller: titleController,
+              onSubmitted: (_)=>_submitForm(),
               decoration: const InputDecoration(
                 labelText: 'Titulo',
               ),
             ),
             TextField(
               controller: valueController,
+              keyboardType:
+                  const TextInputType.numberWithOptions(decimal: true),
+                  onSubmitted: (_)=>_submitForm(),
               decoration: const InputDecoration(
                 labelText: 'Valor (R\$)',
               ),
             ),
             TextButton(
-              onPressed: () {
-               final Title = titleController.text;
-                final value = double.parse(valueController.text)?? 0.0;
-                onSubmit(Title, value);
-              },
+              onPressed: _submitForm,
               child: const Text(
                 "Nova Transação",
                 style: TextStyle(color: Colors.purple),
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
+import 'package:despesa/componentes/transation_forme.dart';
 import 'package:despesa/componentes/transation_user.dart';
-
+import 'package:despesa/modes/transacao.dart';
 import 'package:flutter/material.dart';
+import 'componentes/transation_forme.dart';
+import 'componentes/transation_list.dart';
+import 'dart:math';
 
 main() => runApp(Despesa());
 
@@ -15,17 +19,63 @@ class Despesa extends StatelessWidget {
   }
 }
 
-class Myhomepage extends StatelessWidget {
+class Myhomepage extends StatefulWidget {
   Myhomepage({super.key});
+
+  @override
+  State<Myhomepage> createState() => _MyhomepageState();
+}
+
+class _MyhomepageState extends State<Myhomepage> {
+  final _trasactions = [
+    Transacao(
+      id: 't1',
+      title: 'Novo tênis de corrida',
+      value: 310.76,
+      date: DateTime.now(),
+    ),
+    Transacao(
+      id: 't2',
+      title: 'Conta de luz',
+      value: 200,
+      date: DateTime.now(),
+    ),
+  ];
+
+  _addTransaction(String title, double value) {
+    final newTransaction = Transacao(
+        id: Random().nextDouble().toString(),
+        title: title,
+        value: value,
+        date: DateTime.now());
+
+    setState(() {
+      _trasactions.add(newTransaction);
+    });
+  }
+
+  _opentransactionFormodal(BuildContext context) {
+    showModalBottomSheet(
+        context: context,
+        builder: (_) {
+          return TransationForme(_addTransaction);
+        });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text("despesas pessoais"),
-          backgroundColor: Colors.blue,
-        ),
-        body: Column(
+      appBar: AppBar(
+        title: const Text("despesas pessoais"),
+        backgroundColor: Colors.blue,
+        actions: [
+          IconButton(onPressed: ()=>_opentransactionFormodal(context),
+          
+           icon: const Icon(Icons.add_box_outlined))
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
             Container(
@@ -35,8 +85,14 @@ class Myhomepage extends StatelessWidget {
                 elevation: 10,
               ),
             ),
-            TransationUser(),
+            TransationList(transation: _trasactions),
           ],
-        ));
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: ()=>_opentransactionFormodal(context),
+        child: const Icon(Icons.add_box_outlined),
+      ),
+    );
   }
 }
